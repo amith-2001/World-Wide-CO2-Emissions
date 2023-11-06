5e4f5427cbb83c7bb53a8377cadb229be8e9fffa
amith-2001 <amithr707@gmail.com>
2023-11-05 20:22:09 -0500

    Animate Button added

--- D3js-Visualization/index.js
+++ D3js-Visualization/index.js
@@ -10,6 +10,7 @@ const svgHeight = window_dims.width / 3;
 const World_map = "./data/world_data.geojson";
 const emissions = "./data/emissions_processed.csv";
 const yearSlider = d3.select('#yearSlider');
+const animateButton = d3.select('#animateButton');
 
 try {
     Promise.all([
@@ -23,6 +24,7 @@ try {
 
         const generateMap = (geo_data, containerName, width, height, year, margin = 30) => {
             d3.select(containerName).selectAll("svg").remove();
+            console.log(typeof year);
             const svg = d3.select(containerName).append("svg")
                 .attr("width", width)
                 .attr("height", height);
@@ -30,25 +32,63 @@ try {
             const geoPath_generator = d3.geoPath()
                 .projection(d3.geoMercator().fitSize([width - margin, height - margin], geo_data));
 
-            const colorScale = d3.scaleSequential(d3.interpolateBlues)
-                .domain(d3.extent(geo_data.features, (d) => d.properties.emission_data[year]));
+            const colorInterpolator = d3.interpolateRgbBasis(['#d73027', '#fc8d59', '#fee090', '#e0f3f8', '#91bfdb', '#4575b4'].reverse())
+
+            const linearScale = d3.scaleLinear()
+                .domain(d3.extent(geo_data.features, (d) => {
+                    return d['properties']['emission_data'][year]
+                }))
 
             svg.selectAll("path")
                 .data(geo_data.features)
                 .enter()
                 .append("path")
                 .attr("d", d => geoPath_generator(d))
-                .attr("fill", d => colorScale(d.properties.emission_data[year]));
+                .attr("fill", d => colorInterpolator(linearScale(d['properties']['emission_data'][year])));
+        }
+
+        function updateMap(year) {
+            const colorInterpolator = d3.interpolateRgbBasis(['#d73027', '#fc8d59', '#fee090', '#e0f3f8', '#91bfdb', '#4575b4'].reverse())
+            const linearScale = d3.scaleLinear()
+                .domain(d3.extent(geojson_data.features, (d) => {
+                    return d['properties']['emission_data'][year]
+                }))
+
+            d3.selectAll("path")
+                .attr("fill", d => colorInterpolator(linearScale(d['properties']['emission_data'][year])));
         }
 
         yearSlider.on('input', () => {
-            currentYear = parseInt(yearSlider.property('value'));
-            generateMap(geojson_data, '.map', svgWidth, svgHeight, currentYear);
+            currentYear = +yearSlider.property('value');
+            updateMap(currentYear);
         });
+
+        function animateMap() {
+            if (animationRunning) {
+                clearInterval(interval);
+                animationRunning = false;
+                animateButton.text('Animate');
+            } else {
+                interval = setInterval(() => {
+                    if (currentYear <= endYear) {
+                        updateMap(currentYear);
+                        yearSlider.property('value', currentYear);
+                        currentYear++;
+                    } else {
+                        clearInterval(interval);
+                        animationRunning = false;
+                        animateButton.text('Animate');
+                    }
+                }, 100); // Change this value to adjust animation speed (in milliseconds)
+                animationRunning = true;
+                animateButton.text('Stop');
+            }
+        }
+        animationRunning = false;
+        animateButton.on('click', animateMap);
 
         generateMap(geojson_data, '.map', svgWidth, svgHeight, currentYear);
     });
 } catch (error) {
     console.error("An error occurred:", error);
-    // You can add error handling code here if needed.
 }
